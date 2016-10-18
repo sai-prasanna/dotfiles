d22b3b8c81ae42c646572bdeec79ca4ff462218a
Sai Prasanna R <sai.r.prasanna@gmail.com>
2016-10-18 20:53:41 +0530

    Add flycheck

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,11 +9,9 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
+             '("MELPA Stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
 
 
 ;; Load and activate emacs packages. Do this first so that the
@@ -79,6 +77,9 @@
     
     ;; Web mode
     web-mode
+
+    ;; Flycheck
+    flycheck
     ))
 
 ;; On OS X, an Emacs instance started from the graphical user
@@ -144,3 +145,17 @@
 (load "setup-clojure.el")
 (load "setup-js.el")
 (load "setup-mitscheme.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck web-mode tagedit smex rainbow-delimiters projectile paredit monokai-theme magit ido-ubiquitous company-tern clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
