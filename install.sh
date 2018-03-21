ccd2ab0f09e766d1000feb1f412e885ce8b0e6c9
sai-prasanna <sai.r.prasanna@gmail.com>
2018-03-21 15:44:48 +0530

    Add spacemacs and tmux.conf

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 echo "Backing up existing dotfiles"
-tar -zhcvf dotfilesbackup.tar.gz ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d
-rm -ri  ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d
+tar -zhcvf dotfilesbackup.tar.gz ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d ~/.tmux.conf
+rm -ri  ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d ~/.tmux.conf
 echo "Cloning Repo to ~/.dotfiles"
-git clone https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
+git clone  --recurse-submodules -j8 https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
 echo "Symlinking dotfiles, folders"
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/emacs.d ~/.emacs.d
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 echo "Downloading vundle"
 git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
 echo "Installing Plugins"
--- zshrc
+++ zshrc
@@ -84,3 +84,14 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+
+# TMUX
+if which tmux >/dev/null 2>&1; then
+    # if no session is started, start a new session
+    test -z ${TMUX} && tmux
+    # when quitting tmux, try to attach
+    while test -z ${TMUX}; do
+        tmux attach || break
+    done
+fi
