78bc728073dee669dd867c6cc13df03906ecc1c9
Sai <sai-prasanna@users.noreply.github.com>
2019-03-23 23:42:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ if [ -x "$(command -v apt)" ]; then
 fi
 
 echo "Backing up existing dotfiles"
-tar -zhcvf dotfilesbackup.tar.gz  ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs
+tar -zcvf dotfilesbackup.tar.gz  ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs
 rm -ri ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs
 echo "Cloning Repo to ~/.dotfiles"
-git clone  --recurse-submodules -j8 https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
+git clone  --recurse-submodules https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
 echo "Symlinking dotfiles, folders"
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/emacs.d ~/.emacs.d
