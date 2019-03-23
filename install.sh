715dccefd28308c0a58e91ca0024c370334523b9
sai-prasanna <sai.r.prasanna@gmail.com>
2019-03-23 22:28:52 +0530

    Joining spacemacs forever .. :p

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
+
+if [ -x "$(command -v apt)" ]; then
+    sudo apt install zsh tmux emacs
+fi
+
 echo "Backing up existing dotfiles"
-tar -zhcvf dotfilesbackup.tar.gz ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d ~/.tmux.conf
-rm -ri  ~/.vim ~/.vimrc ~/.zshrc ~/.emacs.d ~/.tmux.conf
+tar -zhcvf dotfilesbackup.tar.gz  ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs
+rm -ri ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs
 echo "Cloning Repo to ~/.dotfiles"
 git clone  --recurse-submodules -j8 https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
 echo "Symlinking dotfiles, folders"
 ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-echo "Downloading vundle"
-git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
-echo "Installing Plugins"
-vim +PluginInstall +qall
-mkdir -p ~/.vim/{backup_files,swap_files,undo_files}
+ln -s ~/.dotfiles/spacemacs ~/.spacemacs
