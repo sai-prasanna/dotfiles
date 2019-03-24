617c32cf72298270c6560d80b6dcfb789ac2c12e
sai-prasanna <sai.r.prasanna@gmail.com>
2019-03-24 22:39:43 +0530

    Change stuff

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ if [ -x "$(command -v apt)" ]; then
 fi
 
 echo "Backing up existing dotfiles"
-tar -zcvf dotfilesbackup.tar.gz  ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs ~/.spacemacs.d
+tar -zcvf dotfilesbackup.tar.gz  ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs.d
 rm -ri ~/.zshrc ~/.emacs.d ~/.tmux.conf ~/.spacemacs ~/.spacemacs.d
 echo "Cloning Repo to ~/.dotfiles"
-git clone  --recurse-submodules https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
+git clone  https://github.com/sai-prasanna/dotfiles.git ~/.dotfiles
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+
 echo "Symlinking dotfiles, folders"
 ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/spacemacs ~/.spacemacs
 ln -s ~/.dotfiles/spacemacs.d ~/.spacemacs.d
--- zshrc
+++ zshrc
@@ -9,6 +9,8 @@ plugins=(git)
 source $ZSH/oh-my-zsh.sh
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
+export TERM=xterm-256color
+
 
 # Editor Setup
 
